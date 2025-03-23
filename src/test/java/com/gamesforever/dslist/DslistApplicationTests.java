d6e5f0461a35d274cd6f6c86b25cdf0070da38b8
JocelioNobre1983 <JocelioNobre1983>
2025-03-23 17:27:51 -0300

    Project created

---
+++ src/test/java/com/gamesforever/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesforever.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
